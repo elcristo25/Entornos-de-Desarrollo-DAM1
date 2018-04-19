e971493896f2e239af48cd74d13a6804b3890437
elcristo25 <christianpd25@gmail.com>
2018-04-19 15:08:01 +0200

    Primer commit con la clase inicial
    Es el primer commit donde agrego la clase principal, con el nombre de los participantes y una clase inicial con sólo el método main.

---
+++ Entornos de desarrollo Colaboracion/src/MainClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @author DAM120 Christian Piñera Diez
+ * @author DAM123 Jesus Ruiz Alonso
+ * @author DAM128 Mario Fernandez Diez
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+	}
+}
