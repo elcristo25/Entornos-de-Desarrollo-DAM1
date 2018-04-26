34732abbeff5f28132eeee6483847c62ea44de59
elcristo25 <christianpd25@gmail.com>
2018-04-26 16:04:45 +0200

    .
    .

--- Entornos de desarrollo Colaboracion/src/CapaDeDatos/SimuladorBaseDeDatos.java
+++ Entornos de desarrollo Colaboracion/src/CapaDeDatos/SimuladorBaseDeDatos.java
@@ -8,4 +8,15 @@ public class SimuladorBaseDeDatos {
 	ArrayList<Tramo> tramos;
 	ArrayList<Tarea> tareas;
 	
+	public String InsertarJornada(Jornada _nuevaJornada) {
+		return this.jornadas.add(_nuevaJornada) ? "OK" : "Error al insetar"; 
+	}
+	
+	public String InsertarTramo(Tramo _nuevoTramo) {
+		return this.tramos.add(_nuevoTramo) ? "OK" : "Error al insetar"; 
+	}
+	
+	public String InsertarTarea(Tarea _nuevaTarea) {
+		return this.tareas.add(_nuevaTarea) ? "OK" : "Error al insetar"; 
+	}
 }
