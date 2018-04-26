901cc45ec44b68f5416c1e27a348062cde1f5481
Morio97 <morionoja@gmail.com>
2018-04-26 14:53:12 +0200

    Revert "Creacion de get y set"
    This reverts commit c3afe260312bc626bd82c306d03189ba4f4fdb08.

--- Entornos de desarrollo Colaboracion/src/Pago.java
+++ Entornos de desarrollo Colaboracion/src/Pago.java
@@ -6,29 +6,5 @@ public class Pago {
 	private Date fecha;
 	private int hora;
 	private char metodoPago;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public Date getFecha() {
-		return fecha;
-	}
-	public void setFecha(Date fecha) {
-		this.fecha = fecha;
-	}
-	public int getHora() {
-		return hora;
-	}
-	public void setHora(int hora) {
-		this.hora = hora;
-	}
-	public char getMetodoPago() {
-		return metodoPago;
-	}
-	public void setMetodoPago(char metodoPago) {
-		this.metodoPago = metodoPago;
-	}
 	
 }
--- Entornos de desarrollo Colaboracion/src/Trabajador.java
+++ Entornos de desarrollo Colaboracion/src/Trabajador.java
@@ -6,41 +6,4 @@ private String apellidos;
 private String direccion;
 private String Telefono;
 private boolean senior;
-public String getNif() {
-	return nif;
-}
-public void setNif(String nif) {
-	this.nif = nif;
-}
-public String getNombre() {
-	return nombre;
-}
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
-public String getApellidos() {
-	return apellidos;
-}
-public void setApellidos(String apellidos) {
-	this.apellidos = apellidos;
-}
-public String getDireccion() {
-	return direccion;
-}
-public void setDireccion(String direccion) {
-	this.direccion = direccion;
-}
-public String getTelefono() {
-	return Telefono;
-}
-public void setTelefono(String telefono) {
-	Telefono = telefono;
-}
-public boolean isSenior() {
-	return senior;
-}
-public void setSenior(boolean senior) {
-	this.senior = senior;
-}
-
 }
