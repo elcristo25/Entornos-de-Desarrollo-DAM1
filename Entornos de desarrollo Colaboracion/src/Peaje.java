41ba3b7dd3037fb74693cf00988f48bbc22162f2
Morio97 <morionoja@gmail.com>
2018-04-26 15:20:07 +0200

    creacion de get y set y creacion de id en todas las clases
    Creacion de gets y sets en las clases con lo que faltaba y creacion en todas las clases de un id de tipo entero.

--- Entornos de desarrollo Colaboracion/src/Peaje.java
+++ Entornos de desarrollo Colaboracion/src/Peaje.java
@@ -4,5 +4,24 @@ public class Peaje {
 	private int id;
 	private int punto;
 	private char sentido;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPunto() {
+		return punto;
+	}
+	public void setPunto(int punto) {
+		this.punto = punto;
+	}
+	public char getSentido() {
+		return sentido;
+	}
+	public void setSentido(char sentido) {
+		this.sentido = sentido;
+	}
 
+	
 }
--- Entornos de desarrollo Colaboracion/src/TCarretera.java
+++ Entornos de desarrollo Colaboracion/src/TCarretera.java
@@ -1,4 +1,13 @@
 
 public class TCarretera {
+private int id;
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
 
 }
